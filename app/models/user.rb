260d1c1206f3508f5f09607a26de579e617fd308
niuli <niuli1991@gmail.com>
2015-12-21 22:36:38 +0800

    replace editor

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :blogs
+  has_many :pictures
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 #  mount_uploader :picture, PictureUploader
